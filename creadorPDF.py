94a64a8e79e4ab6866b52523005e3ad1fe40a06a
LuisOToro <lotoro@espol.edu.ec>
2017-06-11 23:17:29 -0500

    cambios 2
    ahora esta mas corto la parte del parrafo

--- creadorPDF.py
+++ creadorPDF.py
@@ -5,35 +5,8 @@ from reportlab.lib.enums import *
 from reportlab.lib.colors import *
 def Titulo(c,tit):
     styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Forma',alignment = TA_CENTER,
-allowOrphans = 0,
-allowWidows = 1,
-backColor = None,
-borderColor = None,
-borderPadding = 0,
-borderRadius = None,
-borderWidth = 0,
-bulletAnchor = "start",
-bulletFontName = "Times-Roman",
-bulletFontSize = 10,
-bulletIndent = 0,
-endDots = None,
-firstLineIndent = 0,
-fontName = "Times-Roman",
-fontSize = 16,
-justifyBreaks = 0,
-justifyLastLine = 0,
-leading = 12,
-leftIndent = 0,
-rightIndent = 0,
-spaceAfter = 30,
-spaceBefore = 0,
-spaceShrinkage = 0.05,
-splitLongWords = 1,
-textColor = black,
-textTransform = None,
-underlineProportion = 0.0,
-wordWrap = None))
+    styles.add(ParagraphStyle(name='Forma',alignment = TA_CENTER,bulletAnchor = "start", bulletFontName = "Times-Roman",
+    bulletFontSize = 10,fontName = "Times-Roman", fontSize = 16, spaceAfter = 30, spaceBefore = 0,))
     p = Paragraph(tit, styles["Forma"])
     aw = 550
     ah = 770
@@ -45,37 +18,10 @@ wordWrap = None))
         raise ValueError
 def Texto(str1,str2):
     styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Forma1', alignment=TA_LEFT,
-                              allowOrphans=0,
-                              allowWidows=1,
-                              backColor=None,
-                              borderColor=None,
-                              borderPadding=0,
-                              borderRadius=None,
-                              borderWidth=0,
-                              bulletAnchor="start",
-                              bulletFontName="Times-Roman",
-                              bulletFontSize=10,
-                              bulletIndent=0,
-                              endDots=None,
-                              firstLineIndent=40,
-                              fontName="Times-Roman",
-                              fontSize=12,
-                              justifyBreaks=0,
-                              justifyLastLine=0,
-                              leading=12,
-                              leftIndent=30,
-                              rightIndent=0,
-                              spaceAfter=10,
-                              spaceBefore=5,
-                              spaceShrinkage=0.05,
-                              splitLongWords=1,
-                              textColor=black,
-                              textTransform=None,
-                              underlineProportion=0.0,
-                              wordWrap=None))
+    styles.add(ParagraphStyle(name='Forma1', alignment=TA_LEFT, bulletAnchor="start", bulletFontName="Times-Roman", bulletFontSize=10,
+    firstLineIndent=35, fontName="Times-Roman", fontSize=12, leftIndent=30, spaceAfter=10, spaceBefore=5))
     p = Paragraph(str2, styles["Forma1"])
-    aw = 550
+    aw = 500
     ah = 750 - p.getSpaceAfter()
     w, h = p.wrap(aw, ah)
     if w <= aw and h <= ah:
