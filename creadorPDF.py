2dc76cf26ef3655cb70522c8a795032e667cb156
Jonathan <jiquinta@espol.edu.ec>
2017-06-13 03:01:11 -0500

    modificacion de formato ieee, creacion de metodo para add mail ide y autor y modificacion de metodos titulo y texto

--- creadorPDF.py
+++ creadorPDF.py
@@ -1,37 +1,89 @@
 from reportlab.pdfgen import canvas
-from reportlab.platypus import Paragraph
+from reportlab.platypus import Paragraph, SimpleDocTemplate
 from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle,PropertySet
 from reportlab.lib.enums import *
 from reportlab.lib.colors import *
-def Titulo(c,tit):
+from reportlab.lib.pagesizes import A4
+
+def IdPost(StrID,canvas):
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='aligment-left', alignment=TA_LEFT,fontName = "Times-Roman", fontSize = 10))
+    p = Paragraph(StrID, styles["aligment-left"])
+    p.wrap(550,820)
+    p.drawOn(canvas, 30, 810)
+
+def Autor (Strauto,canvas,altura):
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='autort', alignment=TA_CENTER, fontName="Times-Roman", fontSize=10))
+    p = Paragraph(Strauto, styles["autort"])
+    aw = 550
+    ah = altura-20 - p.getSpaceAfter()
+    w, h = p.wrap(aw, ah)
+    if w <= aw and h <= ah:
+        p.drawOn(canvas, 0, ah)
+        ah = ah - h
+    else:
+        raise ValueError
+    return ah
+
+def Mail(correo,canvas,altura):
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='correo', alignment=TA_CENTER, bulletAnchor="start", bulletFontName="Times-Roman",
+                              bulletFontSize=10, fontName="Times-Roman", fontSize=10, spaceAfter=30, spaceBefore=0, ))
+    p = Paragraph(correo, styles["correo"])
+    aw = 550
+    ah = altura+20 - p.getSpaceAfter()
+    w, h = p.wrap(aw, ah)
+    if w <= aw and h <= ah:
+        p.drawOn(canvas, 0, ah)
+        ah = ah - h
+    else:
+        raise ValueError
+    return ah
+
+
+def Titulo(canvas,titulo):
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Forma',alignment = TA_CENTER,bulletAnchor = "start", bulletFontName = "Times-Roman",
     bulletFontSize = 10,fontName = "Times-Roman", fontSize = 16, spaceAfter = 30, spaceBefore = 0,))
-    p = Paragraph(tit, styles["Forma"])
+    p = Paragraph(titulo, styles["Forma"])
     aw = 550
-    ah = 770
+    ah = 820 - p.getSpaceAfter()
     w, h = p.wrap(aw, ah)
     if w <= aw and h <= ah:
-        p.drawOn(c, 0, ah)
+        p.drawOn(canvas, 0, ah)
         ah = ah - h
     else:
         raise ValueError
-def Texto(str1,str2):
+    return ah
+
+
+
+def Texto(canvas,texto,altura):
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Forma1', alignment=TA_LEFT, bulletAnchor="start", bulletFontName="Times-Roman", bulletFontSize=10,
     firstLineIndent=35, fontName="Times-Roman", fontSize=12, leftIndent=30, spaceAfter=10, spaceBefore=5))
-    p = Paragraph(str2, styles["Forma1"])
-    aw = 500
-    ah = 750 - p.getSpaceAfter()
+    p = Paragraph(texto, styles["Forma1"])
+    aw = 550
+    ah = altura - p.getSpaceAfter()
     w, h = p.wrap(aw, ah)
     if w <= aw and h <= ah:
-        p.drawOn(str1, 30, ah)
+        p.drawOn(canvas, 0, ah)
         ah = ah - h
     else:
         raise ValueError
-def Generar(str1,str2,str3):
+
+def Generar(idPost,title,texto1,mail,autor,nombrePost):
     # nombre del archivo sin .pdf, titulo, texto
-    c = canvas.Canvas(str1 + ".pdf")
-    Titulo(c,str2)
-    Texto(c,str3)
+    c = canvas.Canvas(nombrePost + ".pdf")
+    IdPost(idPost, c)
+    ahT = Titulo(c,title)
+    ahA = Autor(autor,c,ahT)
+    ahM = Mail(mail,c,ahA)
+    Texto(c,texto1,ahM)
+
+
     c.save()
+
+#Generar("pdfdeprueba","titulo principal","este es el texto del cuerpo")
+Generar("mi ide","este es mi titulo","este es el tetxo del cuerpo","jiquinta@espol.edu.ec","Jonathan Quintana","mipost")
