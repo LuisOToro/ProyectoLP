a39ef9a0e2e0b0ec2e4797714cc5ff4cd4b40d25
LuisOToro <lotoro@espol.edu.ec>
2017-06-25 22:36:19 -0500

    Expresiones
    En este caso estan las epresiones regulares unidas

--- crearPaginas.py
+++ crearPaginas.py
@@ -1,5 +1,5 @@
 from reportlab.lib.styles import ParagraphStyle as PS
-from reportlab.platypus import PageBreak
+from reportlab.platypus import PageBreak,CondPageBreak
 from reportlab.platypus.paragraph import Paragraph
 from reportlab.platypus.doctemplate import PageTemplate, BaseDocTemplate
 from reportlab.platypus.tableofcontents import TableOfContents
@@ -36,7 +36,46 @@ story.append(PageBreak())
 story.append(Paragraph('First heading', h1))
 story.append(Paragraph('Text in first heading', PS('body')))
 story.append(Paragraph('First sub heading', h2))
-story.append(Paragraph('Text in first sub heading', PS('body')))
+story.append(Paragraph('''Text in first sub heading,ESCUELA SUPERIOR POLITÉCNICA DEL LITORAL
+FACULTAD DE INGENIERÍA EN ELECTRICIDAD Y COMPUTACIÓN
+SISTEMAS DE BASES DE DATOS I
+PRIMERA EVALUACIÓN - II TÉRMINO 2015-2016
+Nombre: _____________________________________________ Matrícula: _____________ Paralelo: _____
+Sección 1 (20%):
+Desarrolle el modelo conceptual del siguiente modelo de negocios.
+A usted, como parte del grupo de desarrollo de la empresa MYSOFT, le han pedido que diseñe una base de datos para un cliente que se dedica a rentar villas vacacionales. Dentro de los requerimientos, el cliente ha pedido que sea una aplicación web y que cumpla con las siguientes características:
+Villa: Cada villa tiene un nombre, un id único, una imagen, año de construcción, edad, y algunas características especiales de la siguiente lista: piscina, Jacuzzi, mesa de billar, juegos de mesa, permite mascotas. Una villa puede ser rentada por un precio que puede variar cada noche. Una villa tiene solo un propietario.
+Usuario: Un usuario puede suscribirse al sitio web y alquilar villas. Un usuario tiene nombre, apellido, id único, email y DoB. Un usuario tiene asociada una lista de villas favoritas y él puede decidir después si reservarlas o no. Al rentar una villa, un usuario declara la fecha de inicio y fecha de fin de la reservación. El usuario ingresa a la villa en la fecha de inicio antes mencionada y deja la villa al medio día de la fecha de fin. Para completar la reservación online, un usuario tiene que hacer un depósito no reembolsable del 50% de el precio final calculado. El usuario debe pagar el saldo al dueño de la villa en el momento de ingreso a la villa.
+Reseña: Un usuario puede escribir reseñas sobre las villas que ha rentado antes. Una reseña contiene un rating (1-5) donde 5 es el mejor rating y un comentario que describe la experiencia del usuario durante su estadía.
+Propietario: Es un usuario, quien es dueño de una o más villas. Tiene un número celular asociado. El propietario como cualquier otro usuario puede reservar villas. Además el propietario puede eliminar y agregar villas a la lista de villas que posee. Para propósitos de auditoría, la fecha de cualquier modificación debe ser registrada en el sistema. Un propietario puede definir nuevas características y añadirlas a la lista de características de la villa. El propietario define el precio de estadía por noche para diferentes períodos de tiempo (definido por una fecha inicio y fecha fin)
+NOTA:
+ No olvide que en su modelo conceptual deben constar las entidades, atributos, cardinalidades de entidades y relaciones, relaciones y nombres de las relaciones.
+ Utilice la información de la sección 3 para realizar el modelo conceptual.
+Sección 2 (30%):
+A partir del modelo conceptual de la sección 1, grafique el modelo lógico completamente normalizado.
+NOTA:
+● No olvide que en su modelo lógico normalizado deben constar las tablas, 
+1
+ESCUELA SUPERIOR POLITÉCNICA DEL LITORAL
+FACULTAD DE INGENIERÍA EN ELECTRICIDAD Y COMPUTACIÓN
+SISTEMAS DE BASES DE DATOS I
+PRIMERA EVALUACIÓN - II TÉRMINO 2015-2016
+Nombre: _____________________________________________ Matrícula: _____________ Paralelo: _____
+Sección 1 (20%):
+Desarrolle el modelo conceptual del siguiente modelo de negocios.
+A usted, como parte del grupo de desarrollo de la empresa MYSOFT, le han pedido que diseñe una base de datos para un cliente que se dedica a rentar villas vacacionales. Dentro de los requerimientos, el cliente ha pedido que sea una aplicación web y que cumpla con las siguientes características:
+Villa: Cada villa tiene un nombre, un id único, una imagen, año de construcción, edad, y algunas características especiales de la siguiente lista: piscina, Jacuzzi, mesa de billar, juegos de mesa, permite mascotas. Una villa puede ser rentada por un precio que puede variar cada noche. Una villa tiene solo un propietario.
+Usuario: Un usuario puede suscribirse al sitio web y alquilar villas. Un usuario tiene nombre, apellido, id único, email y DoB. Un usuario tiene asociada una lista de villas favoritas y él puede decidir después si reservarlas o no. Al rentar una villa, un usuario declara la fecha de inicio y fecha de fin de la reservación. El usuario ingresa a la villa en la fecha de inicio antes mencionada y deja la villa al medio día de la fecha de fin. Para completar la reservación online, un usuario tiene que hacer un depósito no reembolsable del 50% de el precio final calculado. El usuario debe pagar el saldo al dueño de la villa en el momento de ingreso a la villa.
+Reseña: Un usuario puede escribir reseñas sobre las villas que ha rentado antes. Una reseña contiene un rating (1-5) donde 5 es el mejor rating y un comentario que describe la experiencia del usuario durante su estadía.
+Propietario: Es un usuario, quien es dueño de una o más villas. Tiene un número celular asociado. El propietario como cualquier otro usuario puede reservar villas. Además el propietario puede eliminar y agregar villas a la lista de villas que posee. Para propósitos de auditoría, la fecha de cualquier modificación debe ser registrada en el sistema. Un propietario puede definir nuevas características y añadirlas a la lista de características de la villa. El propietario define el precio de estadía por noche para diferentes períodos de tiempo (definido por una fecha inicio y fecha fin)
+NOTA:
+ No olvide que en su modelo conceptual deben constar las entidades, atributos, cardinalidades de entidades y relaciones, relaciones y nombres de las relaciones.
+ Utilice la información de la sección 3 para realizar el modelo conceptual.
+Sección 2 (30%):
+A partir del modelo conceptual de la sección 1, grafique el modelo lógico completamente normalizado.
+NOTA:
+● No olvide que en su modelo lógico normalizado deben constar las tablas, columnas, tipos de datos, claves primarias, claves foráneas, campos obligatorios, campos opcionales y''', PS('body')))
+story.append(CondPageBreak(height=50))
 story.append(PageBreak())
 story.append(Paragraph('Second sub heading', h2))
 story.append(Paragraph('Text in second sub heading', PS('body')))
--- expresiones.py
+++ expresiones.py
@@ -1,15 +1,22 @@
 import re
 #Book IEEE
-exp_pages = '(pp. )(([0-9]+)(-)([0-9]+)|[0-9]+)'
-exp_author = '^([A-Z](. )){,2}[a-zA-Z]+'
-exp_coma = '[,][ ]+'
-exp_title = '((")[a-zA-Z ]+(,")( in )[a-zA-Z ]+|[a-zA-Z ]+)'
+exp_pages = '(pp. )(([0-9]+)(-)([0-9]+)|[0-9]+)[.]'
+exp_author = '^(([A-Z](. )){,2}[a-zA-Z]+|[a-zA-Z ]+)'
+exp_coma = '[(,)([ ]+)]?'
+exp_title = '((")[a-zA-Z ]+(,")( in )?[a-zA-Z ]+|[a-zA-Z ]+|(")[\w]+("))'
 exp_year = '[0-9]+'
 exp_chapter = '(ch. )[0-9]+'
 exp_section = '(sec. )[0-9]+'
-exp_edition = '[0-9]+[a-z]+[ ](ed)'
+exp_edition = '[0-9]+[a-z]+[ ](ed;)'
 exp_volume = '(vol. )[0-9]+'
 exp_city='[a-zA-Z ]+'
-exp_country = '[a-zA-Z ]+(: )[a-zA-Z]+'
+exp_country = '[a-zA-Z ]+(:)[a-zA-Z ]+'
 exp_state = '[A-Z]+'
-
+exp_number = '(no. )[0-9]+'
+exp_month = '[a-zA-Z]+(.)?[ ][0-9]+'
+exp_articleNumber = '(Art. no. )[0-9]+'
+libro = exp_author+exp_coma+exp_title+exp_coma+exp_city+exp_coma+exp_state+exp_coma+exp_country+exp_year+exp_coma+exp_chapter+exp_coma+exp_section+exp_pages
+articulo = exp_author+exp_coma+exp_title+exp_coma+exp_volume+exp_coma+exp_number+exp_coma+exp_month+exp_year+exp_coma+exp_articleNumber
+manual = exp_author+exp_coma+exp_title+exp_coma+exp_city+exp_coma+exp_state+exp_coma+exp_country+exp_coma+exp_year
+conferencia = exp_author+exp_coma+exp_title+exp_coma+exp_city+exp_coma+exp_state+exp_coma+exp_country+exp_coma+exp_year
+publicacion = exp_author+exp_coma+exp_title+exp_coma+exp_pages
